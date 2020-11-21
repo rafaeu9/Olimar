c7e86ba743b96f55c948f5b167ea28271ee872bb
AaronSolent <aaronsolent@gmail.com>
2020-11-21 16:35:57 +0000

    fixed Object Rotation not resetting

--- Olimar/Assets/Scripts/ResetManager.cs
+++ Olimar/Assets/Scripts/ResetManager.cs
@@ -6,18 +6,21 @@ public class ResetManager : MonoBehaviour
 {
 	private Rigidbody2D rb2d;
 	private Vector3 startPos;
+	private Quaternion startRot;
 	private GameManager game;
 	// Start is called before the first frame update
 	void Start()
 	{
 		rb2d = GetComponent<Rigidbody2D>();
 		startPos = transform.position;
+		startRot = transform.rotation;
 		game = GameManager.instance;
 	}
 	
 	public void StartSim()
 	{
 		startPos = transform.position;
+		startRot = transform.rotation;
 		rb2d.bodyType = RigidbodyType2D.Dynamic;
 	}
     
@@ -26,6 +29,7 @@ public class ResetManager : MonoBehaviour
 		rb2d.velocity = Vector3.zero;
 		rb2d.bodyType = RigidbodyType2D.Static;
 		transform.position = startPos;
+		transform.rotation = startRot;
 	}
 
     // Update is called once per frame
